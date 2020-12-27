d996912d1b8e6214dcc20b3190f0d68ea9a6195a
Austin Deric <austin.deric@gmail.com>
2020-12-27 14:30:38 -0600

    remap joint state publisher

--- spot_viz/launch/view_model.launch.py
+++ spot_viz/launch/view_model.launch.py
@@ -7,12 +7,18 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 import os
 
 def generate_launch_description():
+    bringup_dir = get_package_share_directory('spot_viz')
     return LaunchDescription([
         Node(
             package='joint_state_publisher_gui',
             namespace='joint_state_publisher_gui',
             executable='joint_state_publisher_gui',
-            name='joint_state_publisher_gui'
+            name='joint_state_publisher_gui',
+            remappings=[
+                ('/joint_state_publisher_gui/joint_states', '/joint_states'),
+                ('/joint_state_publisher_gui/robot_description', '/robot_description'),
+                ('/joint_state_publisher_gui/parameter_events', '/parameter_events')
+            ]
         ),
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource(
@@ -20,5 +26,12 @@ def generate_launch_description():
                              'launch',
                              'description.launch.py')
             )
+        ),
+        Node(
+            package='rviz2',
+            namespace='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            arguments=['-d', os.path.join(bringup_dir,'rviz', 'robot.rviz"')],
         )
     ])
