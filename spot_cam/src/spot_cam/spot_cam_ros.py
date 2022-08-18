cfb2e3ac67cfd36e7dd2b76c4624fa2804b69bc8
Michal Staniaszek <m.staniaszek@gmail.com>
2022-08-18 11:45:04 +0100

    simple led brightness control, only able to set all leds to same value

--- spot_cam/src/spot_cam/spot_cam_ros.py
+++ spot_cam/src/spot_cam/spot_cam_ros.py
@@ -1,6 +1,33 @@
 import rospy
 import logging
 from spot_cam.spot_cam_wrapper import SpotCamWrapper
+from std_msgs.msg import Float32MultiArray, Float32
+
+
+class LightingHandler:
+    def __init__(self, wrapper: SpotCamWrapper):
+        self.lighting_client = wrapper.lighting
+        self.led_publisher = rospy.Publisher(
+            "spot/cam/leds", Float32MultiArray, latch=True, queue_size=1
+        )
+        self.led_subscriber = rospy.Subscriber(
+            "spot/cam/set_leds", Float32, callback=self.leds_callback
+        )
+        # Allow time for publisher to initialise before publishing led state
+        rospy.sleep(0.3)
+        self._publish_leds()
+
+    def leds_callback(self, msg):
+        self.set_leds(msg.data)
+
+    def set_leds(self, brightness):
+        self.lighting_client.set_led_brightness(brightness)
+        self._publish_leds()
+
+    def _publish_leds(self):
+        self.led_publisher.publish(
+            Float32MultiArray(data=self.lighting_client.get_led_brightness())
+        )
 
 
 class SpotCam:
@@ -23,9 +50,11 @@ class SpotCam:
 
         self.logger = logging.getLogger("rosout")
 
-        wrapper = SpotCamWrapper(
+        self.wrapper = SpotCamWrapper(
             self.hostname, self.username, self.password, self.logger
         )
+
+        self.lighting = LightingHandler(self.wrapper)
 
     def main(self):
         rospy.spin()
--- spot_cam/src/spot_cam/spot_cam_wrapper.py
+++ spot_cam/src/spot_cam/spot_cam_wrapper.py
@@ -1,7 +1,51 @@
-import logging
+import enum
+import typing
+
 import bosdyn.client
 from bosdyn.client import spot_cam
+from bosdyn.client.spot_cam.lighting import LightingClient
+
 from spot_driver.spot_wrapper import SpotWrapper
+
+
+class SpotCamLightingHandler:
+    """
+    Handler for interacting with LED brightnesses
+    """
+    class LEDPosition(enum.Enum):
+        """
+        Values indicate the position of the specified LED in the brightness list
+        """
+
+        REAR_LEFT = 0
+        FRONT_LEFT = 1
+        FRONT_RIGHT = 2
+        REAR_RIGHT = 3
+
+    def __init__(self, robot):
+        self.client = robot.ensure_client(LightingClient.default_service_name)
+
+    def set_led_brightness(self, brightness):
+        """
+        Set the brightness of the LEDs to the specified brightness
+
+        Args:
+            brightness: LEDs will all be set to this brightness, which should be in the range [0, 1]. The value will
+                        be clipped if outside this range.
+        """
+        # Clamp brightness to [0,1] range
+        brightness = min(max(brightness, 0), 1)
+        self.client.set_led_brightness([brightness] * 4)
+
+    def get_led_brightness(self) -> typing.List[float]:
+        """
+        Get the brightness of the LEDS
+
+        Returns:
+            List of floats indicating current brightness of each LED, in the order they are specified in the
+            LEDPosition enum
+        """
+        return self.client.get_led_brightness()
 
 
 class SpotCamWrapper:
@@ -16,4 +60,8 @@ class SpotCamWrapper:
         spot_cam.register_all_service_clients(sdk)
 
         robot = sdk.create_robot(self._hostname)
-        SpotWrapper.authenticate(robot, self._hostname, self._username, self._password, self._logger)
+        SpotWrapper.authenticate(
+            robot, self._hostname, self._username, self._password, self._logger
+        )
+
+        self.lighting = SpotCamLightingHandler(robot)
