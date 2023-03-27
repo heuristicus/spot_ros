96a500922d1f054811f74bdeaa203946d57b0e04
Bahaa Aldeeb <aldeeb.bahaa@gmail.com>
2023-03-27 14:02:38 -0500

    relocates world_object_wrapper.

--- spot_driver/setup.py
+++ spot_driver/setup.py
@@ -2,7 +2,11 @@ from setuptools import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-    packages=["spot_driver"], scripts=["scripts/spot_ros", 'scripts/world_object_wrapper_test'], package_dir={"": "src"}
+    packages=["spot_driver"], 
+    scripts=["scripts/spot_ros", 
+             'scripts/examples/world_object_wrapper_test', 
+             ], 
+    package_dir={"": "src"}
 )
 
 setup(**d)
