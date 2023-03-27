7d83a9721a40871ac2ba17f82585f563f0a33d6c
Bahaa Aldeeb <aldeeb.bahaa@gmail.com>
2023-03-27 14:02:38 -0500

    First version of world_objects_wrapper.

--- spot_driver/setup.py
+++ spot_driver/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-    packages=["spot_driver"], scripts=["scripts/spot_ros"], package_dir={"": "src"}
+    packages=["spot_driver"], scripts=["scripts/spot_ros", 'scripts/world_object_wrapper_test'], package_dir={"": "src"}
 )
 
 setup(**d)
