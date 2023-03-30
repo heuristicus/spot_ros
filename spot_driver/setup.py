93f03c8f7b0c6af031503eb3a51741ab7c691cdd
Bahaa Aldeeb <aldeeb.bahaa@gmail.com>
2023-03-30 15:34:05 -0500

    Updates example code.
    Sets up for testing the hand command functions of the spot wrapper.

--- spot_driver/setup.py
+++ spot_driver/setup.py
@@ -5,7 +5,8 @@ d = generate_distutils_setup(
     packages=["spot_driver"], 
     scripts=["scripts/spot_ros", 
              'scripts/examples/world_object_wrapper_test', 
-             'scripts/examples/highlevel_command_wrapper_test',
+             'scripts/examples/task_wrapper_example',
+             'scripts/examples/hand_pose_example',
              ], 
     package_dir={"": "src"}
 )
